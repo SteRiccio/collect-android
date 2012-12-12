9db2380f3abc4a9eef2c8534a693a1fc7a71c252
Alexey Voronov <alexey.voronov@arbonaut.com>
2012-12-12 10:31:22 +0200

    add virtual keyboard support and datetime pickers

---
+++ android-app/src/main/java/org/openforis/collect/android/dialogs/DateSetDialog.java
@@ -0,0 +1,93 @@
+package org.openforis.collect.android.dialogs;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.openforis.collect.android.R;
+import org.openforis.collect.android.fields.DateField;
+import org.openforis.collect.android.tabs.TabManager;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.DatePicker;
+
+public class DateSetDialog extends FragmentActivity {
+	
+	//Subclass which creates DatePicker
+	public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+		public DateField activity_edittext;
+		public DatePickerDialog datePicker;
+		
+		public DatePickerFragment(DateField date_field) {
+		    activity_edittext = date_field;
+		}	    
+		
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current date as the default date in the picker
+			final Calendar c = Calendar.getInstance();
+			int year = c.get(Calendar.YEAR);
+			int month = c.get(Calendar.MONTH);
+			int day = c.get(Calendar.DAY_OF_MONTH);
+			
+			// Create a new instance of DatePickerDialog and return it
+			this.datePicker = new DatePickerDialog(getActivity(), this, year, month, day);
+			
+			//Add listener for button "Cancel"
+			this.datePicker.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.cancel), new DialogInterface.OnClickListener() {
+			    public void onClick(DialogInterface dialog, int which) {
+			       if (which == DialogInterface.BUTTON_NEGATIVE) {
+			          //Finish activity
+			    	   finish();
+			       }
+			    }
+			});			
+			return this.datePicker;
+		}
+		
+		public void onDateSet(DatePicker view, int year, int month, int day) {
+			//Set date to the clicked DateField 
+			Calendar cal = new GregorianCalendar(year, month, day);
+			String strDate = DateFormat.getDateFormat(getActivity()).format(cal.getTime());
+			Log.i(getResources().getString(R.string.app_name), "Date in DateSetDialog is: " + strDate);
+			activity_edittext.txtBox.setText(strDate);	
+		    //Finish activity
+		    finish();
+		}
+		
+	}
+	
+	//Main class
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    getWindow().requestFeature(Window.FEATURE_OPTIONS_PANEL);
+	    Bundle extras = getIntent().getExtras(); 
+	    if (extras != null) {
+	    	  int widget_id = extras.getInt("datefield_id");
+	    	  Log.i(getResources().getString(R.string.app_name), "Id of target field in DateSetDialog is: " + widget_id);
+	    	  View v = TabManager.getUIElement(widget_id);
+	    	  if (v != null){
+//	    		  Log.i(getResources().getString(R.string.app_name), "View for DateSetDialog is: " +v.toString());
+	    		  showDatePickerDialog((DateField)v);
+	    	  }
+	    	  else
+	    		  Log.i(getResources().getString(R.string.app_name), "View is NULL!");
+	    	  
+	    }    
+	    
+	}	
+	
+	public void showDatePickerDialog(DateField dateField) {
+	    DialogFragment newFragment = new DatePickerFragment(dateField);
+	    newFragment.show(getSupportFragmentManager(), "datePicker");
+	}	
+}
---
+++ android-app/src/main/java/org/openforis/collect/android/dialogs/TimeSetDialog.java
@@ -0,0 +1,96 @@
+package org.openforis.collect.android.dialogs;
+
+import java.util.Calendar;
+
+import org.openforis.collect.android.R;
+import org.openforis.collect.android.fields.TimeField;
+import org.openforis.collect.android.tabs.TabManager;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.TimePicker;
+
+public class TimeSetDialog extends FragmentActivity {
+	public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+		
+		public TimeField activity_edittext;
+		public TimePickerDialog timePicker;
+		
+		public TimePickerFragment(TimeField time_field){
+		    activity_edittext = time_field;			
+		}
+		
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current time as the default values for the picker
+			final Calendar c = Calendar.getInstance();
+			int hour = c.get(Calendar.HOUR_OF_DAY);
+			int minute = c.get(Calendar.MINUTE);
+			
+			// Create a new instance of TimePickerDialog and return it
+			this.timePicker = new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+			
+			//Add listener for button "Cancel"
+			this.timePicker.setButton(DialogInterface.BUTTON_NEGATIVE, getString(R.string.cancel), new DialogInterface.OnClickListener() {
+			    public void onClick(DialogInterface dialog, int which) {
+			       if (which == DialogInterface.BUTTON_NEGATIVE) {
+			          //Finish activity
+			    	  finish();
+			       }
+			    }
+			});					
+			return this.timePicker;
+		}
+
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+		    //Set time to the clicked TimeField
+			String strTime = pad(hourOfDay) + ":" + pad(minute);
+			Log.i(getResources().getString(R.string.app_name), "Time in TimeSetDialog is: " + strTime);
+			activity_edittext.txtBox.setText(strTime);			
+			//Finish activity
+		    finish();
+		}
+		
+		//Transform values bellow 10 as 01, 02 etc
+		private String pad(int c) {
+		    if (c >= 10)
+		        return String.valueOf(c);
+		    else
+		        return "0" + String.valueOf(c);
+		}		
+	}
+	
+	//Main class
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    getWindow().requestFeature(Window.FEATURE_OPTIONS_PANEL);
+	    Bundle extras = getIntent().getExtras(); 
+	    if (extras != null) {
+	    	  int widget_id = extras.getInt("timefield_id");
+	    	  Log.i(getResources().getString(R.string.app_name), "Id of target field in TimeSetDialog is: " + widget_id);
+	    	  View v = TabManager.getUIElement(widget_id);
+	    	  if (v != null){
+//	    		  Log.i(getResources().getString(R.string.app_name), "View for TimeSetDialog is: " +v.toString());
+	    		  showTimePickerDialog((TimeField)v);
+	    	  }
+	    	  else
+	    		  Log.i(getResources().getString(R.string.app_name), "View is NULL!");
+	    	  
+	    }    
+	    
+	}	
+	
+	public void showTimePickerDialog(TimeField timeField) {
+	    DialogFragment newFragment = new TimePickerFragment(timeField);
+	    newFragment.show(getSupportFragmentManager(), "timePicker");
+	}	
+}
