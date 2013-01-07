85968b5062b8ce6c16811329b02d77942e9e153e
karol <karol.waga@arbonaut.com>
2013-01-07 12:17:16 +0200

    removal of scrolling arrows

--- android-app/src/main/java/org/openforis/collect/android/misc/SwipeDetector.java
+++ android-app/src/main/java/org/openforis/collect/android/misc/SwipeDetector.java
@@ -19,14 +19,14 @@ public class SwipeDetector extends SimpleOnGestureListener {
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         try {
-            if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+            /*if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
                 return false;
             //right to left swipe
             if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
                	this.uiElement.scrollRight();
             }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
             	this.uiElement.scrollLeft();
-            }
+            }*/
         } catch (Exception e) {
           
         }
