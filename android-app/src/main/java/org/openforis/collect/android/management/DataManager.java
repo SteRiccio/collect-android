cbf4f4f0716c55d3763d3aea6285b28188669f3f
karol <karol.waga@arbonaut.com>
2013-05-02 15:25:08 +0300

    changes in project dependencies

--- android-app/src/main/java/org/openforis/collect/android/management/DataManager.java
+++ android-app/src/main/java/org/openforis/collect/android/management/DataManager.java
@@ -152,9 +152,9 @@ public class DataManager {
 			JdbcDaoSupport.close();
 		} catch (NullPointerException e){
 			e.printStackTrace();
-		} catch (RecordPersistenceException e) {
+		} /*catch (RecordPersistenceException e) {
 			e.printStackTrace();
-		}
+		}*/
 		Log.e("record"+recordId,"LOADED IN "+(System.currentTimeMillis()-startTime)/1000+"s");
 		return loadedRecord;
 	}
