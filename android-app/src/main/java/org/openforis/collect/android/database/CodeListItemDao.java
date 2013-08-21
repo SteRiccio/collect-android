a060f3fa64457e6a24a4952d646d1b4227a66f4c
Alexey Voronov <alexey.voronov@arbonaut.com>
2013-08-21 09:28:05 +0300

    Fix for optimization

--- android-app/src/main/java/org/openforis/collect/android/database/CodeListItemDao.java
+++ android-app/src/main/java/org/openforis/collect/android/database/CodeListItemDao.java
@@ -5,24 +5,27 @@ import static org.openforis.collect.persistence.jooq.tables.OfcCodeList.OFC_CODE
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jooq.Record;
+//import org.jooq.SelectQuery;
 import org.jooq.TableField;
-import org.openforis.collect.android.management.ApplicationManager;
+import org.openforis.collect.android.config.Configuration;
 import org.openforis.collect.model.CollectSurvey;
 import org.openforis.collect.persistence.jooq.tables.records.OfcCodeListRecord;
-import org.openforis.idm.metamodel.CodeList;
-import org.openforis.idm.metamodel.CodeListItem;
-import org.openforis.idm.metamodel.LanguageSpecificText;
-import org.openforis.idm.metamodel.ModelVersion;
-import org.openforis.idm.metamodel.PersistedCodeListItem;
-
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
-//import org.jooq.SelectQuery;
 //import org.jooq.Record;
 //import org.jooq.Result;
 //import org.jooq.SelectQuery;
 //import org.openforis.collect.persistence.CodeListItemDao.JooqFactory;
+import org.openforis.idm.metamodel.CodeList;
+import org.openforis.idm.metamodel.ModelVersion;
+import org.openforis.idm.metamodel.PersistedCodeListItem;
+import org.openforis.idm.metamodel.Survey;
+import org.openforis.idm.metamodel.SurveyObject;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 
 public class CodeListItemDao extends org.openforis.collect.persistence.CodeListItemDao {
@@ -33,9 +36,9 @@ public class CodeListItemDao extends org.openforis.collect.persistence.CodeListI
 	
 	@Override
 	protected List<PersistedCodeListItem> loadChildItems(CodeList codeList, Integer parentItemId, ModelVersion version) {
-		long startTime = System.currentTimeMillis();
 		List<PersistedCodeListItem> result = new ArrayList<PersistedCodeListItem>();
-		//Log.e("Mobile DAO", "Starts loading child item: " + System.currentTimeMillis());	
+		long startTime = System.currentTimeMillis();
+		Log.e("Mobile DAO", "Starts loading child item: " + startTime);	
 		//Prepare query
 		CollectSurvey survey = (CollectSurvey) codeList.getSurvey();
 		TableField<OfcCodeListRecord, Integer> surveyIdField = getSurveyIdField(survey.isWork());		
@@ -53,56 +56,75 @@ public class CodeListItemDao extends org.openforis.collect.persistence.CodeListI
 				+ " and " + parentIdCondition
 				+ " order by " + OFC_CODE_LIST.SORT_ORDER; 
 		
-		//Log.e("Mobile DAO", "Query is: " + query);
-		Log.e("Mobile DAO", codeList.getId()+"CodeList is: " + codeList.getName());
+		Log.d("Mobile DAO", "Query is: " + query);
 		//Execute query
 		SQLiteDatabase db = DatabaseHelper.getDb();
 		Cursor cursor = db.rawQuery(query, null);
-		//Log.e("Mobile DAO", "Number of rows is: " + cursor.getCount());
+		Log.d("Mobile DAO", "Number of rows is: " + cursor.getCount());
 		//Close database
 		db.close();
 		//Prepare result
-		while (cursor.moveToNext()) {
-			/*for (int i=0;i<cursor.getColumnCount();i++){
-				Log.e(cursor.getColumnName(i)+"=","=="+cursor.getString(i));
-			}*/
-			CodeListItem codeListItem = new CodeListItem(codeList, codeList.getId());
-			//codeListItem.setAnnotation(qname, value);
-			//Log.e("CODE","=="+cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.CODE.getName())));
-			codeListItem.setCode(cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.CODE.getName())));
-			//codeListItem.setDeprecatedVersion(deprecated);
-			//codeListItem.setDeprecatedVersionByName(name);
-			//codeListItem.setLabel(language, text);
-			//codeListItem.setParentItem(parentItem);
-			codeListItem.setQualifiable(Boolean.valueOf(cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.QUALIFIABLE.getName()))));
-			//codeListItem.setSinceVersion(since);
-			//codeListItem.setSinceVersionByName(name);
-			//codeListItem.addDescription(description);
-			
-			//LanguageSpecificText label = new LanguageSpecificText(ApplicationManager.selectedLanguage,cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.LABEL1.getName())));
-			//codeListItem.addLabel(label);
-			List<String> languageList = codeList.getSurvey().getLanguages();
-			//Log.e("languagesNo",languageList.get(0)+"=="+languageList.size());
-			//Log.e("LABEL","=="+cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.LABEL1.getName())));
-			LanguageSpecificText label = new LanguageSpecificText(languageList.get(0),cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.LABEL1.getName())));
-			codeListItem.addLabel(label);
-			if (cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.LABEL2.getName()))!=null){
-				//Log.e("LABEL","=="+cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.LABEL2.getName())));
-				label = new LanguageSpecificText(languageList.get(1),cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.LABEL2.getName())));
-				codeListItem.addLabel(label);
-			}
-			if (cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.LABEL3.getName()))!=null){
-				//Log.e("LABEL","=="+cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.LABEL3.getName())));
-				label = new LanguageSpecificText(languageList.get(2),cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.LABEL3.getName())));
-				codeListItem.addLabel(label);
-			}
-			
-			PersistedCodeListItem item = PersistedCodeListItem.fromItem(codeListItem);
-			result.add(item);
-		}
-		//Log.e("Mobile DAO", "Ready to return child item: " + System.currentTimeMillis());
-		Log.e("MOBILE DAO", "Total time: "+(System.currentTimeMillis()-startTime));
+		Log.e("Mobile DAO", "PrepareResults: " + System.currentTimeMillis());
+		int itemId;
+		PersistedCodeListItem entity;
+		if (cursor.moveToFirst()){
+			do{
+				itemId = cursor.getInt(cursor.getColumnIndex(OFC_CODE_LIST.ITEM_ID.getName()));
+				entity = new PersistedCodeListItem(codeList, itemId);
+				entity.setSystemId(cursor.getInt(cursor.getColumnIndex(OFC_CODE_LIST.ID.getName())));
+				entity.setSortOrder(cursor.getInt(cursor.getColumnIndex(OFC_CODE_LIST.SORT_ORDER.getName())));
+				entity.setCode(cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.CODE.getName())));
+				entity.setParentId(cursor.getInt(cursor.getColumnIndex(OFC_CODE_LIST.PARENT_ID.getName())));
+				entity.setQualifiable(Boolean.parseBoolean(cursor.getString(cursor.getColumnIndex(OFC_CODE_LIST.QUALIFIABLE.getName()))));
+				entity.setSinceVersion(extractModelVersion(entity, cursor.getInt(cursor.getColumnIndex(OFC_CODE_LIST.SINCE_VERSION_ID.getName()))));
+				entity.setDeprecatedVersion(extractModelVersion(entity, cursor.getInt(cursor.getColumnIndex(OFC_CODE_LIST.DEPRECATED_VERSION_ID.getName()))));
+				extractLabels(codeList, cursor, entity);
+				extractDescriptions(codeList, cursor, entity);			
+				
+				result.add(entity);
+			}while(cursor.moveToNext());
+		}	
+		
+		Log.e("Mobile DAO", "Ready to return child item: " + System.currentTimeMillis());
+		Log.e("Mobile DAO", "Total time: " + (System.currentTimeMillis() - startTime));
 		return result;
 	}
 	
+	protected ModelVersion extractModelVersion(SurveyObject surveyObject, Integer versionId) {
+		Survey survey = surveyObject.getSurvey();
+		ModelVersion version = versionId == null ? null: survey.getVersionById(versionId);
+		return version;
+	}
+	
+	protected void extractLabels(CodeList codeList, Cursor crs, PersistedCodeListItem item) {
+		Survey survey = codeList.getSurvey();
+		item.removeAllLabels();
+		List<String> languages = survey.getLanguages();
+//		Log.e("Mobile DAO", "Set labels. Number of languages is: " + languages.size() + " SurveyId is " + survey.getId());
+		String[] labelColumnNames = {OFC_CODE_LIST.LABEL1.getName(), OFC_CODE_LIST.LABEL2.getName(), OFC_CODE_LIST.LABEL3.getName()};
+		for (int i = 0; i < languages.size(); i++) {
+			String lang = languages.get(i);
+			String label = crs.getString(crs.getColumnIndex(labelColumnNames[i]));
+//			Log.e("Mobile DAO", "Set label: " + label + " for language: " + lang);
+			item.setLabel(lang, label);
+			if(i >=3)
+				break;
+		}
+	}
+	
+	protected void extractDescriptions(CodeList codeList, Cursor crs, PersistedCodeListItem item) {
+		Survey survey = codeList.getSurvey();
+		item.removeAllDescriptions();
+		List<String> languages = survey.getLanguages();
+//		Log.e("Mobile DAO", "Set description. Number of languages is: " + languages.size() + " SurveyId is " + survey.getId());
+		String[] descrColumnNames = {OFC_CODE_LIST.DESCRIPTION1.getName(), OFC_CODE_LIST.DESCRIPTION2.getName(), OFC_CODE_LIST.DESCRIPTION3.getName()};
+		for (int i = 0; i < languages.size(); i++) {
+			String lang = languages.get(i);
+			String label = crs.getString(crs.getColumnIndex(descrColumnNames[i]));
+//			Log.e("Mobile DAO", "Set description: " + label + " for language: " + lang);
+			item.setDescription(lang, label);
+			if(i >=3)
+				break;
+		}
+	}
 }
--- android-app/src/main/java/org/openforis/collect/android/management/CodeListManager.java
+++ android-app/src/main/java/org/openforis/collect/android/management/CodeListManager.java
@@ -9,8 +9,6 @@ import org.openforis.idm.metamodel.CodeListItem;
 import org.openforis.idm.metamodel.ExternalCodeListItem;
 import org.openforis.idm.metamodel.PersistedCodeListItem;
 
-import android.util.Log;
-
 public class CodeListManager extends org.openforis.collect.manager.CodeListManager{
 	
 	
@@ -43,16 +41,31 @@ public class CodeListManager extends org.openforis.collect.manager.CodeListManag
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T extends CodeListItem> List<T> loadChildItems(CodeListItem parent) {
-		Log.e("MOBILE","loadChildItems");
+		System.err.println("Load child items from mobile CodeListManager");
 		CodeList list = parent.getCodeList();
 		if ( list.isExternal() ) {
 			return (List<T>) getProvider().getChildItems((ExternalCodeListItem) parent);
 		} else if ( list.isEmpty() ) {
+			System.err.println("Finish loading child items from mobile CodeListManager from CodeListDao");
 			return (List<T>) getCodeListItemDao().loadChildItems((PersistedCodeListItem) parent);
 		} else {
+			System.err.println("Finish loading child items from mobile CodeListManager from parent.getChildItems");
 			return parent.getChildItems();
 		}
 	}
-
-
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T extends CodeListItem> List<T> loadRootItems(CodeList list) {
+		System.err.println("Load root items from mobile CodeListManager");
+		if ( list.isExternal() ) {
+			return (List<T>) provider.getRootItems(list);
+		} else if ( list.isEmpty() ) {
+			System.err.println("Finish loading root items from mobile CodeListManager from CodeListDao");
+			return (List<T>) codeListItemDao.loadRootItems(list);
+		} else {
+			System.err.println("Finish loading items from mobile CodeListManager from list.getItems");
+			return list.getItems();
+		}
+	}
 }
